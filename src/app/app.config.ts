c8988ca2a62d919f371c57f364d7a9d53ae37113
rahman.muraman <rahman.muraman@gmail.com>
2024-08-13 09:58:36 +0700

    feat: add global state

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,27 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 import { authInterceptor } from './interceptor/auth.interceptor';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideEffects } from '@ngrx/effects';
+import { paginationReducer } from './state/pagination/pagination.reducer';
+import { reposReducer } from './state/repos/repos.reducers';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withHashLocation()), 
+    provideRouter(routes, withHashLocation()),
     provideClientHydration(),
-    provideHttpClient(
-      withFetch(),
-      withInterceptors([
-        authInterceptor
-      ])
-    ),
-  ]
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
+    provideStore({ pagination: paginationReducer, repos: reposReducer }),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    provideEffects(),
+  ],
 };
